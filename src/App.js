5762467d46e49db1eefa8a45d4a1cd10b6b5463b
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-08-14 21:40:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App=()=>{
+  const design=(
+    <div id="mainContainer">
+      <div id="page"></div>
+    </div>
+  );
+  return design;
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+const Navbar=()=>{
+    const design=(
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Images</a></li>
+            <li><a href="#">Developer</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Contact us</a></li>
+        </ul>
+    );
+    return design;
+}
+
+export default Navbar;
